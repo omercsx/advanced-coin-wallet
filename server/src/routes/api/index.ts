bd84ebd942cf9ecb04c87c630b8a7b2dc45f975d
Arif Damar <arif660damar@gmail.com>
2022-05-19 15:58:01 +0300

    format

--- server/src/routes/api/index.ts
+++ server/src/routes/api/index.ts
@@ -8,9 +8,7 @@ import { SuccessResult } from "../../models/result";
 const router = express.Router();
 
 router.get("/", (_req: express.Request, res: express.Response) => {
-  return res
-    .status(200)
-    .json(new SuccessResult("API is up and running!", null));
+  return res.status(200).json(new SuccessResult("API is up and running!", null));
 });
 
 router.use("/auth", authRoutes);
