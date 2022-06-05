4643d08d73a55f1d6c6746bf94d766f90b4024c4
Arif Damar <arif.damar@ogr.sakarya.edu.tr>
2022-06-05 15:58:40 +0300

    add request logging and move health check to root

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,6 +1,16 @@
 import express from "express";
-import apiRouter from './api';
+import { SuccessResult } from "../models/result";
+import apiRouter from "./api";
 const router = express.Router();
+
+router.get("/", (_req: express.Request, res: express.Response) => {
+  return res.status(200).json(new SuccessResult("API is up and running!", null));
+});
+
+router.use((req, res, next) => {
+  console.log(`${new Date().toUTCString()} - ${req.method} ${req.url}`);
+  next();
+});
 
 router.use("/api", apiRouter);
 
